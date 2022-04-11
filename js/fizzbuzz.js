0538c3de6c15c731a4b1c848899e237797c9d95f
Cory Tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-04-11 18:02:28 -0400

    Delete js directory

--- js/fizzbuzz.js
+++
@@ -1,9 +0,0 @@
-function fizzBuzz(num) {
-    for ( var i = 1; i <= num; i++) {
-        if (i % 5 === 0 && i % 3 === 0) console.log(`${i} Fizz Buzz`);
-        else if (i % 5 === 0) console.log(`${i} Buzz`);
-        else if (i % 3 === 0) console.log (`${i} Fizz`);
-        else console.log(i);
-    }
-}
-fizzBuzz(100);
