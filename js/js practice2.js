3b57bf0351c4f4f9dde3817e431e16a494e653f6
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-04-12 10:55:40 -0400

    media-update

--- js/js practice2.js
+++
@@ -1,8 +0,0 @@
-var sample = "Hello Codeup";
-
-var reSample = sample.indexOf('C');
-var reSample2 = sample.indexOf('u');
-console.log(reSample);
-console.log(reSample2);
-
-sample.substring(reSample, reSample2);
