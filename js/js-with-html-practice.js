b727db141054b6f694699f3635009d83f110786e
Cory Tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-04-12 10:59:23 -0400

    Delete js directory

--- js/js-with-html-practice.js
+++
@@ -1,5 +0,0 @@
-var myName = "Cory";
-var myAge = 39;
-var isMonday = true;
-console.log(myName, myAge, isMonday);
-console.log("my Name: " + myName + " my Age: " + myAge + " is it "  + "monday? " + isMonday); //concatenation
